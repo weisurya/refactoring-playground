046d799ad47de202f41327b5d90e90ae5fe13df8
Soen, Surya Soenaryo <wei.surya@gmail.com>
2019-06-04 22:21:53 +0700

    First commit

---
+++ ch1/01_starting_point/01_starting_program.js
@@ -0,0 +1,73 @@
+'use strict';
+
+const plays = require('./plays.json');
+const invoices = require('./invoices.json');
+
+function statement(invoice, plays) {
+
+    let totalAmount = 0;
+    
+    let volumeCredits = 0;
+    
+    let result = `Statement for ${invoice.customer}\n`
+
+    const format = new Intl.NumberFormat("en-US",
+                            {
+                                style: "currency", 
+                                currency: "USD",
+                                minimumFractionDigits: 2
+                            }).format;
+    
+    for(let perf of invoice.performances) {
+
+        const play = plays[perf.playID];
+
+        let thisAmount = 0;
+
+        switch(play.type) {
+            case "tragedy":
+                thisAmount = 40000;
+                
+                if(perf.audience > 30) {
+                    thisAmount += 1000 * (perf.audience - 30);
+                }
+
+                break;
+            
+            case "comedy":
+                thisAmount = 30000;
+
+                if(perf.audience > 20) {
+                    thisAmount += 10000 + 500 * (perf.audience - 20);
+                }
+
+                thisAmount += 300 * perf.audience;
+
+                break;
+
+            default:
+                throw new Error(`Unknown type: ${play.type}`);
+        }
+
+        // Add volume credits
+        volumeCredits += Math.max(perf.audience - 30, 0);
+
+        // Add extra credit for every ten comedy attendees
+        if("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+
+        // Print line for this order
+        result += ` ${play.name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`;
+
+        totalAmount += thisAmount;
+    }
+
+    result += `Amount owed is ${format(totalAmount / 100)}\n`;
+
+    result += `You earned ${volumeCredits} credits\n`;
+
+    return result;
+}
+
+// Run the code
+const result = statement(invoices[0], plays);
+console.log(result);
