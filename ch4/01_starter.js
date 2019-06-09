81e00c9ef4f811eb1701eb7177b8e5448c144261
Soen, Surya Soenaryo <wei.surya@gmail.com>
2019-06-09 18:02:01 +0700

    Add test - no producer, zero demand, neg. demand

--- ch4/01_starter.js
+++ ch4/01_starter.js
@@ -108,4 +108,39 @@ describe('province', () => {
         expect(asia.shortfall).equal(-6);
         expect(asia.profit).equal(292);
     })
+    it('zero demand', () => {
+        asia.demand = 0;
+
+        expect(asia.shortfall).equal(-25);
+        expect(asia.profit).equal(0);
+    })
+    it('negative demand', () => {
+        asia.demand = -1;
+
+        expect(asia.shortfall).equal(-26);
+        expect(asia.profit).equal(-10);
+    })
+})
+
+describe('no producers', () => {
+    let noProducers;
+
+    beforeEach(() => {
+        const data = {
+            name: 'No producers',
+            producers: [],
+            demand: 30,
+            price: 20,
+        };
+
+        noProducers = new Province(data);
+    });
+
+    it('shortfall', () => {
+        expect(noProducers.shortfall).equal(30);
+    })
+
+    it('profit', () => {
+        expect(noProducers.profit).equal(0);
+    })
 })
